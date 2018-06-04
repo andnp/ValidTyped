f4b4fc4d6dd59db03ebe5bc5bc2655ca46be17d4
andy.patterson <andy.patterson@genesys.com>
2018-06-04 19:56:34 -0400

    feat: make validation errors available

---
+++ tests/integration/scale.test.ts
@@ -0,0 +1,26 @@
+import v from 'index';
+import { pass } from '../helpers/assert';
+
+test('Ajv compiles only one instance of schema', () => {
+    const validator = v.object({
+        a: v.string([ 'a', 'b', 'c' ]),
+        b: v.number(),
+        c: v.boolean(),
+    });
+
+    const t = Date.now();
+    for (let i = 0; i < 2e4; ++i) {
+        const o = {
+            a: 'a',
+            b: Math.random(),
+            c: false,
+        };
+
+        if (validator.isValid(o)) pass();
+        else fail();
+    }
+
+    const took = Date.now() - t;
+
+    expect(took).toBeLessThan(1000);
+});
